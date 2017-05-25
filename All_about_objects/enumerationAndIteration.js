8a94a9886f8ef31d0ec17a952c753420406b33e3
sks092016 <subhashsoni403@gmail.com>
2017-05-25 17:04:00 +0530

    Added another file

--- All_about_objects/enumerationAndIteration.js
+++ All_about_objects/enumerationAndIteration.js
@@ -31,3 +31,88 @@ console.log(Object.keys(obj)) ; // ["a"] not b as it is not enumerable
 //    above method
 
 console.log(Object.getOwnPropertyNames(obj)) ; // ["a", "b"]
+
+// iteration over objects
+
+
+try {
+	for (var v of obj){
+		console.log(v) ;
+	}
+} catch (e) {
+	console.log("We didn't fund a iterator") ;
+}
+
+// for ... of loop looks for the @@iterator function for the object property to iterate over which is built 
+// in for arrays which can be demonstrat as follows
+
+// In contrast to the above the there has been many function or iterators are available for the array elements 
+// however for Objects we have only some like for.....in but even that also does not iterate over the enumerable 
+// elements. For arrays we have like for....in , for...of , forEach() , every() , some()
+
+var myArray = ["akash", "gaurav", "avinash"] ;
+
+for (var v of myArray){
+	console.log(v) ;
+} 
+  // akash
+  // gaurav
+  // avinash
+
+for (var v in myArray){
+	console.log(myArray[v]) ;
+}
+  // akash
+  // gaurav
+  // avinash
+
+function hi(el){
+console.log("hi "+ el);
+}
+ 
+myArray.forEach(hi);
+
+myArray.some(hi) ;
+
+myArray.every(hi) ; 
+
+// we have seen above that the for of loop does not iterate over the object as it require a inerator for doing iteration 
+// as well as a next() function which internally points to the next object property
+
+// in the following code we are going to define our own iterator 
+
+var obj2 = {
+	a : "Hello",
+	b : "How are you"	
+}
+
+Object.defineProperty(obj2, Symbol.iterator, {writable: false , configurable : true , enumerable : false , 
+	value : function(){
+						var Obj = this ; 
+						var objPropertyArray = Object.keys(Obj) ;
+						var index = 0 ;
+
+						return {
+								next : function (){
+
+												   return {
+																value : function(){
+																		
+																		return Obj[objPropertyArray[index++]] + " Abishek" ;
+																}(), 
+																done : (index > objPropertyArray.length) 
+
+													   	    } ;
+												 
+												 } 
+							  } ; 
+						}  } ); 
+
+
+
+for (var v of obj2 )
+{
+	console.log(v) ;
+}
+
+
